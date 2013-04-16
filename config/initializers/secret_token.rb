00cf86424ecb75d7a243fa46d00926125ce180b6
EC2 Default User <ec2-user@ip-172-31-26-222.sa-east-1.compute.internal>
2013-04-16 07:39:36 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kasey::Application.config.secret_token = 'cccaa14e2ddc274c39140c18ced47bacdc7687527b23530afb054069edbd3de89ca30ea0bb27523ae16c8b5a7b3a6df9d43dcf3b0f65f3c53ebe8946eba59f4e'
